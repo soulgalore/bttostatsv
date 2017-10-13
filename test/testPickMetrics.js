18d07c723c63057909b43dcc2a51bc995dbec30e
Peter Hedenskog <phedenskog@wikimedia.org>
2017-10-13 14:41:25 +0200

    pick median and mdev by default

--- test/testPickMetrics.js
+++ test/testPickMetrics.js
@@ -11,12 +11,18 @@ const keyStart = 'this.is.my.key';
 test( 'Pick right metrics from Browsertime JSON', t => {
 	const metrics = getMetrics( browsertimeJSON, keyStart );
 	const facit = [
-		'this.is.my.key.visualMetrics.FirstVisualChange=1000ms',
-		'this.is.my.key.visualMetrics.SpeedIndex=1117ms',
-		'this.is.my.key.visualMetrics.VisualComplete85=1050ms',
-		'this.is.my.key.visualMetrics.VisualComplete95=1216ms',
-		'this.is.my.key.visualMetrics.VisualComplete99=2250ms',
-		'this.is.my.key.visualMetrics.LastVisualChange=2250ms'
+		'this.is.my.key.visualMetrics.FirstVisualChange.median=1000ms',
+		'this.is.my.key.visualMetrics.FirstVisualChange.mdev=35ms',
+		'this.is.my.key.visualMetrics.SpeedIndex.median=1117ms',
+		'this.is.my.key.visualMetrics.SpeedIndex.mdev=32ms',
+		'this.is.my.key.visualMetrics.VisualComplete85.median=1050ms',
+		'this.is.my.key.visualMetrics.VisualComplete85.mdev=89ms',
+		'this.is.my.key.visualMetrics.VisualComplete95.median=1216ms',
+		'this.is.my.key.visualMetrics.VisualComplete95.mdev=86ms',
+		'this.is.my.key.visualMetrics.VisualComplete99.median=2250ms',
+		'this.is.my.key.visualMetrics.VisualComplete99.mdev=119ms',
+		'this.is.my.key.visualMetrics.LastVisualChange.median=2250ms',
+		'this.is.my.key.visualMetrics.LastVisualChange.mdev=119ms'
 	];
 	t.deepEqual( metrics, facit );
 } );
